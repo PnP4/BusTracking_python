e88b07ac7f37c53660f16dcd975d5e7cac06657a
knadikari <knadikari@gmail.com>
2016-11-23 15:56:57 +0530

    Added brocker compatability

--- FilterBusData.py
+++ FilterBusData.py
@@ -1,8 +1,19 @@
 import json
+import pika
 
-data=json.loads('{"id":1001,"regno":"300-2050","datamov":[{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589},'
-                '{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589},'
-                '{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589}],"time":125885555,"nooftickets":150,"fuel":15000}')
+#data=json.loads('{"id":1001,"regno":"300-2050","datamov":[{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589},'
+  #              '{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589},'
+   #             '{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589},{"speed":50,"lat":83.045,"lon":79.589}],"time":125885555,"nooftickets":150,"fuel":15000}')
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='filterdata')
+channel.queue_declare(queue='overspeed')
+channel.queue_declare(queue='overwaiting')
+print "hello"
+
 
 def getspeeddata(data):
     id=data["id"]
@@ -29,5 +40,28 @@ def getlocationdata(data):
     extracteddata={"id":id,"regno":regno,"movedata":locations,"time":time}
     return json.dumps(extracteddata)
 
-print getspeeddata(data)
-print getlocationdata(data)
+def callback(ch, method, properties, body):
+    #print getspeeddata(data)
+    #print getlocationdata(data)
+    data = json.loads(body)
+    print data
+    channel.basic_publish(exchange='',
+                          routing_key='overwaiting',
+                        body=json.dumps(getspeeddata(data)))
+
+    channel.basic_publish(exchange='',
+                          routing_key='overwaiting',
+                          body=json.dumps(getlocationdata(data)))
+    
+while True:
+    try:
+        while True:
+            print "1"
+            channel.basic_consume(callback,
+                                  queue='filterdata',
+                                  no_ack=True)
+            channel.start_consuming()
+
+    except Exception, e:
+        print e
+
